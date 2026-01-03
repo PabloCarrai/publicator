b01a54e6eb750618c9947f56fc09ef4f449e1038
PabloCarrai <pablohernanjuan@hotmail.com>
2026-01-03 20:18:29 -0300

    Ahora nadie sabe como se llama mi usuario por defecto. Jajajaja

--- publicator.sh
+++ publicator.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 dc=$(which docker-compose)
-f=/home/ed/wp-w-wpcli/docker-compose.yml
+f=$HOME/wp-w-wpcli/docker-compose.yml
 
 $dc -f $f run --rm wpcli post create --post_type=post --post_title="$1" --post_content="$2" --post_status=publish
 echo "Publicando...."
